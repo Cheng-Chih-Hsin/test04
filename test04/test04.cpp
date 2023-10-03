00073219d1b6d83d2da17d29ff1c827d741f0b46
Cheng-Chih-Hsin <125848354+Cheng-Chih-Hsin@users.noreply.github.com>
2023-10-03 19:24:54 +0800

    加入專案檔案。

---
+++ test04/test04.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef struct listnode
+{
+    int data;
+    struct listnode* next;
+}NODE;
+
+void ListTraverse(listnode* head)
+{
+    listnode* p = head;
+    p = p->next;
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    NODE* listA;
+    listA = new NODE;
+    listA->data = 20;
+    listA->next = NULL;
+    cout << "";
+
+    NODE* listB;
+    listB = new NODE;
+    listB->data = 0;
+    listB->next = new NODE;
+    listB->next->data = 10;
+    listB->next->next = new NODE;
+    listB->next->next->data = 20;
+    listB->next->next->next = new NODE;
+    listB->next->next->next->data = 30;
+    listB->next->next->next->next = new NODE;
+    listB->next->next->next->next->data = 40;
+    listB->next->next->next->next->next = NULL;
+
+    ListTraverse(listB);
+}
